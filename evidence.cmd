4b7e2c3a6689ee29fc37969693b6dc58755a00be
peterju <peterju.tw@gmail.com>
2018-05-11 03:06:39 +0800

    beta  test

--- evidence.cmd
+++ evidence.cmd
@@ -4,6 +4,7 @@ cd /d "%~dp0"
 ::::::::::::::::: 參數設置 :::::::::::::::::
 ::讀取設定檔參數
 FOR /F "tokens=1,2 delims==" %%i IN ('findstr /V [#\[] _config.txt') DO set %%i=%%j
+for /f "tokens=2-8 delims=.:/ " %%a in ("%date% %time: =0%") do set DateNtime=%%a%%b%%d%%e
 SETLOCAL ENABLEDELAYEDEXPANSION
 ::判斷設定檔中所列檔案與目錄是否存在
 set envflag=1
@@ -11,11 +12,20 @@ if not exist %csvFile% set envflag=0&&echo CSV檔案%csvFile%不存在
 if not exist %sourceFolder% set envflag=0&&echo 來源目錄%sourceFolder%不存在
 if not exist %targetFolder% mkdir %targetFolder%
 if not exist %targetFolder% set envflag=0&&echo 匯出目錄%targetFolder%不存在
-if %envflag%==0 goto end
+if %envflag%==0 Exit /b
 
 ::讀取CSV檔
 del /q step*.txt 2>nul
-FOR /F "tokens=1 delims=, skip=1" %%i IN ('type %csvFile%') do @echo %%i>>step1.txt
+FOR /F "tokens=1 delims=, skip=1" %%i IN ('type %csvFile%') do echo %%i>>step1.txt
 powershell -command "type step1.txt | sort -unique">step2.txt
 
-:end
+FOR /F "tokens=1 delims=" %%i IN (step2.txt) do (
+    if exist %sourceFolder%\%%i* (
+        move %sourceFolder%\%%i* %targetFolder%
+    ) else (
+        echo %%i 憑證不存在
+        echo %%i 憑證不存在>>lost%DateNtime%.txt
+    )
+)
+
+pause
